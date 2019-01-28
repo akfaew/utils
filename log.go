deff9b5d605190703a90b1f414e9015bb25f6b97
Michal Mazurek <akfaew@gmail.com>
2019-01-28 16:31:22 +0100

    Add logging functions

--- log.go
+++ log.go
@@ -1,6 +1,9 @@
 package utils
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 var Debug = false
 
@@ -8,4 +11,22 @@ func LogcDebugf(format string, a ...interface{}) {
 	if Debug {
 		log.Printf(format, a...)
 	}
+}
+
+func LogcDebugfd(format string, a ...interface{}) {
+	if Debug {
+		file, line := logctx(1)
+
+		log.Printf("%s:%d %s", file, line, fmt.Sprintf(format, a...))
+	}
+}
+
+func LogcErrorf(format string, a ...interface{}) {
+	log.Printf("ERROR: "+format, a...)
+}
+
+func LogcErrorfd(format string, a ...interface{}) {
+	file, line := logctx(1)
+
+	log.Printf("ERROR: %s:%d %s", file, line, fmt.Sprintf(format, a...))
 }
