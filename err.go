307e29506759497569226e47e92f075adc7f8670
Michal Mazurek <akfaew@gmail.com>
2019-05-20 12:13:41 +0200

    Rename error functions

--- err.go
+++ err.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 )
 
-func Err(format string, a ...interface{}) error {
-	file, line := logctx(1)
-
-	return fmt.Errorf("%s:%d %s", file, line, fmt.Sprintf(format, a...))
+// Errorfc creates a new error and wraps it with context information using Errorc().
+func Errorfc(format string, a ...interface{}) error {
+	return Errorc(fmt.Errorf(format, a...))
 }
 
-func ErrRich(err error) error {
+// Errorc wraps the error, if not nil, with context information, such as the file name
+// and line.
+func Errorc(err error) error {
 	if err != nil {
 		file, line := logctx(1)
 		return fmt.Errorf("%s:%d %s", trim(file), line, err)
