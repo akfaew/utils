4f7bb4ec8d1cac3bda79b37545915cabb4bd7a3a
Michal Mazurek <akfaew@gmail.com>
2019-12-27 10:29:38 +0100

    Add Time.Now()

--- time.go
+++ time.go
@@ -6,13 +6,23 @@ type TimeReal struct{}
 type TimeTest struct{}
 
 type MockTime interface {
+	Now() time.Time
 	Since(t time.Time) time.Duration
 }
 
 var Time MockTime = TimeReal{}
 
+func (_ TimeReal) Now() time.Time {
+	return time.Now()
+}
+
 func (_ TimeReal) Since(t time.Time) time.Duration {
 	return time.Since(t)
+}
+
+func (_ TimeTest) Now() time.Time {
+	now, _ := time.Parse("2006-01-02 15:04:05", "2018-10-29 14:34:45")
+	return now
 }
 
 func (_ TimeTest) Since(t time.Time) time.Duration {
