84f97cecd24385e6c7218a8c69a78dd2851cc856
Michal Mazurek <akfaew@gmail.com>
2023-09-04 05:21:28 +0200

    Add convenience functions

---
+++ appengine.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+var (
+	ErrNoCronHeader     = errors.New("Cron request does not have the X-Appengine-Cron header")
+	ErrNoTasknameHeader = errors.New("Task request does not have the X-Appengine-Taskname header")
+)
+
+func ValidateCron(r *http.Request) error {
+	if r.Header.Get("X-Appengine-Cron") == "" {
+		return ErrNoCronHeader
+	}
+
+	return nil
+}
+
+func ValidateTask(r *http.Request) error {
+	if r.Header.Get("X-Appengine-Taskname") == "" {
+		return ErrNoTasknameHeader
+	}
+
+	return nil
+}
---
+++ emailoctopus.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const (
+	eoapi = "https://emailoctopus.com/api/1.6/"
+)
+
+type EmailOctopus struct {
+	ListID string
+	APIKey string
+}
+
+type eoCreateContactParams struct {
+	APIKey string            `json:"api_key"`
+	Email  string            `json:"email_address"`
+	Status string            `json:"status"`
+	Fields map[string]string `json:"fields"`
+}
+
+// https://emailoctopus.com/api-documentation/lists/create-contact
+func (eo *EmailOctopus) Add(ctx context.Context, profile *Profile) error {
+	params := eoCreateContactParams{
+		APIKey: eo.APIKey,
+		Email:  profile.Email,
+		Status: "SUBSCRIBED",
+	}
+	p, err := json.Marshal(params)
+	if err != nil {
+		return Errorc(err)
+	}
+
+	u := fmt.Sprintf(eoapi+"lists/%s/contacts", eo.ListID)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, bytes.NewBuffer(p))
+	if err != nil {
+		return Errorc(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Errorc(err)
+	}
+	resp.Body.Close()
+
+	return nil
+}
+
+// https://emailoctopus.com/api-documentation/lists/create-contact
+func (eo *EmailOctopus) Remove(ctx context.Context, profile *Profile) error {
+	id := fmt.Sprintf("%x", md5.Sum([]byte(strings.ToLower(profile.Email))))
+
+	u := fmt.Sprintf(eoapi+"lists/%s/contacts/%s?api_key=%s",
+		eo.ListID, id, eo.APIKey)
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, u, nil)
+	if err != nil {
+		return Errorc(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Errorc(err)
+	}
+	resp.Body.Close()
+
+	return nil
+}
