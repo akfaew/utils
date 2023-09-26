10ecac99cfaaebb23ee3c8b5d1933b71631c5d19
Michal Mazurek <akfaew@gmail.com>
2023-09-26 04:03:00 +0200

    Escape html

--- webhandler/funcs.go
+++ webhandler/funcs.go
@@ -1,12 +1,14 @@
 package webhandler
 
 import (
+	"html"
 	"html/template"
 	"strings"
 )
 
 // Convert simple text to html
 func FormatHTML(text string) template.HTML {
+	text = html.EscapeString(text)
 	ps := strings.Split(text, "\n")
 	for i := range ps {
 		if strings.HasPrefix(ps[i], ">") {
