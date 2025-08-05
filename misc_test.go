622032cb768ff1d3eac61153fcd44fdeb8d0277c
Michal Mazurek <akfaew@gmail.com>
2025-08-05 07:35:47 +0200

    retain RootCause helper

--- misc_test.go
+++ misc_test.go
@@ -9,28 +9,6 @@ import (
 	"github.com/akfaew/utils/fixture"
 )
 
-func TestSlash(t *testing.T) {
-	tests := []struct {
-		input string
-		ret1  string
-		ret2  string
-	}{
-		{"", "", ""},
-		{"test", "test", ""},
-		{"test/", "test", ""},
-		{"test///", "test", "//"},
-		{"test/case", "test", "case"},
-		{"test/case/a/b/c", "test", "case/a/b/c"},
-		{"/case/a/b/c", "", "case/a/b/c"},
-	}
-
-	for _, tc := range tests {
-		ret1, ret2 := Slash(tc.input)
-		require.Equal(t, ret1, tc.ret1)
-		require.Equal(t, ret2, tc.ret2)
-	}
-}
-
 func TestRandEmail(t *testing.T) {
 	val := RandEmail()
 	require.Len(t, val, 16)
