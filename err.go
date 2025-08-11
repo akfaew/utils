4f287b0739b049e810ddb6c0f3be08325fad4d37
Michal Mazurek <akfaew@gmail.com>
2025-08-11 10:04:59 +0200

    Simplify Errorc helpers and expand tests

--- err.go
+++ err.go
@@ -26,7 +26,7 @@ func UserErrorfc(err error, format string, a ...any) error {
 	file, line := logctx(1)
 
 	return UserError{
-		Err:         fmt.Errorf("%s:%d %w", trim(file), line, err),
+		Err:         fmt.Errorf("%s:%d %w", file, line, err),
 		UserMessage: fmt.Sprintf(format, a...),
 	}
 }
@@ -34,7 +34,7 @@ func UserErrorfc(err error, format string, a ...any) error {
 func Errorfc(format string, a ...any) error {
 	file, line := logctx(1)
 
-	return fmt.Errorf("%s:%d %w", trim(file), line, fmt.Errorf(format, a...))
+	return fmt.Errorf("%s:%d %w", file, line, fmt.Errorf(format, a...))
 }
 
 func Errorc(err error) error {
@@ -43,7 +43,7 @@ func Errorc(err error) error {
 	}
 	file, line := logctx(1)
 
-	return fmt.Errorf("%s:%d %w", trim(file), line, err)
+	return fmt.Errorf("%s:%d %w", file, line, err)
 }
 
 func RootCause(err error) error {
